888b741e94275f1d0e3a1992b803c44a37339feb
pedro-batista <peddrobatista2022@gmail.com>
2024-02-16 16:43:55 -0300

    Terceiro commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,26 +1,19 @@
 package application;
 
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
+
+import entities.Product;
 
 public class Program {
 	
 	public static void main(String[] args) {
-		Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
-		Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
-		//union
-		Set<Integer> c = new TreeSet<>(a);
-		c.addAll(b);
-		System.out.println(c);
-		//intersection
-		Set<Integer> d = new TreeSet<>(a);
-		d.retainAll(b);
-		System.out.println(d);
-		//difference
-		Set<Integer> e = new TreeSet<>(a);
-		e.removeAll(b);
-		System.out.println(e);
+		Set<Product> set = new HashSet<>();
+		set.add(new Product("TV", 900.0));
+		set.add(new Product("Notebook", 1200.0));
+		set.add(new Product("Tablet", 400.0));
+		Product prod = new Product("Notebook", 1200.0);
+		System.out.println(set.contains(prod));
 	}
 
 }
